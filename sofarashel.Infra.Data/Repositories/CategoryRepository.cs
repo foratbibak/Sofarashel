becf0cd45ea698b31542d1ac638130bc68c7d508
ForatBibak <foratbibak341@gmailc.om>
2026-07-27 16:06:58 +0330

    CategoryRepo Implment

---
+++ sofarashel.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sofarashel.Data;
+using Sofarashel.Domain.Contracts;
+using Sofarashel.Domain.Models.Categories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sofarashel.Infra.Data.Repositories
+{
+    public class CategoryRepository(GallaryDbcontext _context) : ICategoryRepository
+    {
+        public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Category>> GetRootCategoriesAsync()
+        {
+            return await _context.Categories
+                .Where(c => c.ParentId == null)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Category>> GetChildrenAsync(int parentId)
+        {
+            return await _context.Categories
+                .Where(c => c.ParentId == parentId)
+                .ToListAsync();
+        }
+
+        public async Task<Category?> GetByIdAsync(int categoryId)
+        {
+            return await _context.Categories.FindAsync(categoryId);
+        }
+
+        public async Task<Category?> GetByIdForAdminAsync(int? categoryId)
+        {
+            return await _context.Categories
+                .Include(c => c.Children)
+                .Include(c => c.Images)
+                .FirstOrDefaultAsync(c => c.Id == categoryId);
+        }
+
+        public async Task CreateCategoryAsync(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+        }
+
+        public async Task UpdateCategoryAsync(Category category)
+        {
+            _context.Categories.Update(category);
+        }
+
+        public async Task DeleteAsync(Category category)
+        {
+            category.IsDelete = true;
+            category.DeleteDate = DateTime.Now;
+            _context.Categories.Update(category);
+        }
+
+        public async Task DeleteAsync(int categoryId)
+        {
+            var category = await GetByIdAsync(categoryId);
+            await DeleteAsync(category);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
