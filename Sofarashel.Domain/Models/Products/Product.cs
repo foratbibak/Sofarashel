ee60096c75032d42b73ba5c72f14625f61042adb
ForatBibak <foratbibak341@gmailc.om>
2026-09-01 12:24:52 +0330

    Update Product Tbls

--- Sofarashel.Domain/Models/Products/Product.cs
+++ Sofarashel.Domain/Models/Products/Product.cs
@@ -10,18 +10,10 @@ namespace Sofarashel.Domain.Models.Products
 
         public string? Description { get; set; }
 
-        public string? MainImage { get; set; }
-
-        public string? Material { get; set; }
-        public string? FabricType { get; set; }
-        public string? Color { get; set; }
-        public string? Style { get; set; }
-        public decimal? Length { get; set; }
-        public decimal? Width { get; set; }
-        public decimal? Height { get; set; }
-
         #region Relations
-        public ICollection<ProductImage>? Images { get; set; }
+        public ICollection<Rel_Image_Product>? ProductImages { get; set; }
+
+        public ICollection<Rel_AttributesFetures_Product>? ProductAttributes { get; set; }
 
         public ICollection<Rel_Product_Category>? ProductCategories { get; set; }
         #endregion
